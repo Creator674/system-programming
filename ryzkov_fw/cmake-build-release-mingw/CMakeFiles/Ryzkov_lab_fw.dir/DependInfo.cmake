d7f7c81e1e8e4ca83a61095aafd26ca052222868
Yaroslav Apxit3kt0R <666anastyalena@mail.ru>
2021-10-24 23:26:12 +0300

    feat: implement fw lab

---
+++ ryzkov_fw/cmake-build-release-mingw/CMakeFiles/Ryzkov_lab_fw.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/programming/projects/Ryzkov_lab_fw/main.c" "D:/programming/projects/Ryzkov_lab_fw/cmake-build-release-mingw/CMakeFiles/Ryzkov_lab_fw.dir/main.c.obj"
+  "D:/programming/projects/Ryzkov_lab_fw/splpv1.c" "D:/programming/projects/Ryzkov_lab_fw/cmake-build-release-mingw/CMakeFiles/Ryzkov_lab_fw.dir/splpv1.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ryzkov_fw/cmake-build-release-mingw/CMakeFiles/Ryzkov_lab_fw.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ryzkov_lab_fw.dir/main.c.obj"
+  "CMakeFiles/Ryzkov_lab_fw.dir/splpv1.c.obj"
+  "Ryzkov_lab_fw.exe"
+  "Ryzkov_lab_fw.exe.manifest"
+  "Ryzkov_lab_fw.pdb"
+  "libRyzkov_lab_fw.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ryzkov_lab_fw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
